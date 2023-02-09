cb43f9d67fa28e87c635d78bc191101bc18b28f5
Leonardo Peixoto <ljpeixoto@gmail.com>
2023-02-09 07:49:30 -0300

    Criar input-ref.directive para capturar focus e blud do input projetado, e usar para estilizar corretamente au-fa-input quando ganhar ou perder o foco

--- au-input/src/app/lib/au-fa-input/au-fa-input.component.ts
+++ au-input/src/app/lib/au-fa-input/au-fa-input.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, ContentChild, HostBinding, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { InputRefDirective } from '../common/input-ref.directive';
 
 @Component({
   selector: 'au-fa-input',
@@ -6,14 +7,28 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./au-fa-input.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class AuFaInputComponent implements OnInit {
+export class AuFaInputComponent implements OnInit, AfterViewInit {
 
   @Input()
   icon: string;
 
+  @ContentChild(InputRefDirective, {static: false})
+  input: InputRefDirective;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+    if (!this.input) {
+      console.log('the au-fa-input needs an input inside its content');
+    }
+  }
+
+  @HostBinding('class.input-focus')
+  get isInputFocus() {
+    return this.input ? this.input.focus : false;
   }
 
   get classes() {
